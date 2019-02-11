6f0deb6505f6ec7f9afafc8ad3157f1e30ed694c
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-02-12 06:37:54 +0700

    add login and register template

---
+++ application/controllers/Auth/Register.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * 
+ */
+class Register extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->load->view('auth/register/index');
+    }
+
+}
--- application/views/resources/head_resource.php
+++ application/views/resources/head_resource.php
@@ -12,10 +12,10 @@
     <!-- jvectormap -->
     <link rel="stylesheet" href="<?= base_url() ?>assets/vendor/jvectormap/jquery-jvectormap.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?= base_url() ?>assets/vendor/admin-lte/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/vendor/template_admin-lte/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
              folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="<?= base_url() ?>assets/vendor/admin-lte/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/vendor/template_admin-lte/dist/css/skins/_all-skins.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
