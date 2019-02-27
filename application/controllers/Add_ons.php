e47b00c76c91a76c7dbaeb53345a69b9e957abc7
sherli yualinda <42725403+sherliyualinda@users.noreply.github.com>
2019-02-27 22:06:20 +0700

    add ons form

--- application/controllers/Add_ons.php
+++ application/controllers/Add_ons.php
@@ -1,8 +1,5 @@
 <?php 
 
-/**
- * 
- */
 class Add_ons extends MY_Controller{
 	
 	function __construct(){
@@ -12,8 +9,61 @@ class Add_ons extends MY_Controller{
 	public function index()	{
 		$this->load->view('add_ons/index');
 	}
-	public function details($id){
+	public function details($id=0){
 		$data['id'] = $id;
 		$this->load->view('add_ons/details', $data);
+	}
+	public function new_addon(){
+		$this->load->view('add_ons/new_addon');
+	}
+	public function insert_add_on(){
+		$judul 		= $this->input->post('judul');
+		$deskripsi 	= $this->input->post('deskripsi');
+		$harga 		= $this->input->post('harga');
+		$kategori 	= $this->input->post('kategori');
+		$pembuat 	= $this->input->post('pembuat');
+		$screenshot = $this->input->post('screenshot');
+
+		$this->form_validation->set_rules([
+			[
+				'field' => 'judul',
+				'label'	=> 'Judul',
+				'rules' => 'required|min_length[4]'
+			],
+			[
+				'field' => 'deskripsi',
+				'label'	=> 'Deskripsi',
+				'rules' => 'required|min_length[4]'
+			],
+			[
+				'field' => 'harga',
+				'label'	=> 'Harga',
+				'rules' => 'required|min_length[4]|numeric'
+			],
+			[
+				'field' => 'kategori',
+				'label'	=> 'Kategori',
+				'rules' => 'required'
+			],
+			[
+				'field' => 'pembuat',
+				'label'	=> 'Pembuat',
+				'rules' => 'required|min_length[4]'
+			]
+		]);
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('add_ons/new_addon');
+		} else {
+			$data = [
+				'judul' => $judul,
+				'deskripsi' => $deskripsi,
+				'harga' => $harga,
+				'kategori' => $kategori,
+				'pembuat' => $pembuat
+			];
+			$this->load->view('add_ons/details', $data);
+		}
+
 	}
 }
--- application/views/add_ons/details.php
+++ application/views/add_ons/details.php
@@ -17,51 +17,79 @@
   <!-- =============================================== -->
 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+  <div class="content-wrapper" style="float: left;">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
         DETAILS ADD ONS
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
-      </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title <?= $id ?></h3>
-        </div>
-        <div class="box-body">
-          <div class="row">
-            <div class="col-md-12">
-                <img src='<?= base_url() ?>storage/images/kucing.jpg' width='500'>
+      <div class="col-md-10 col-md-offset-1">
+        <div class="box">
+          <div class="box-body">
+            <div class="row">
+              <div class="col-md-3">
+                <img src="<?= base_url('storage/images/memo.jpg') ?>" class="img-responsive">
+              </div>
+              <div class="col-md-9">
+                <h2><?= $judul ?></h2>
+                <span style="font-size: 30px; font-weight: 700; position: absolute; top: 10px; right: 20px;"><?=$harga?></span>
+                <p style="font-size: 16px;">Creator : <?= $pembuat ?></p>
+                <p style="font-size: 16px;">27 February 2019</p>
+                <p style="font-size: 16px;">Rating</p>
+                <button class="btn btn-success">BUY</button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12"> 
+                <p align="justify"><?= $deskripsi?> </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <p style="font-size:20px" align="center">Sreenshoots</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <center>
+                <img src="<?= base_url('storage/images/memo.jpg')?>" width="400px" height="300px">
+                <P>Gambar 1</P>
+                </center>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <nav aria-label="Page navigation" class="col-md-offset-4">
+                  <ul class="pagination">
+                    <li class="disabled">
+                      <a href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                      </a>
+                    </li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                      <a href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+                <!-- end nav -->
+              </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-12">
-                <p>sjhsdjgfhsdbchjdsgcuhecjkndiojiowionjkgwuiqgiowudopjscnxjkcbsugdioewhfioscsa</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-                <button class="btn btn-success pull-right">Add</button>
-            </div>
-          </div>
+          <!-- End Box Body -->
         </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
       </div>
-      <!-- /.box -->
 
     </section>
     <!-- /.content -->
@@ -69,10 +97,6 @@
   <!-- /.content-wrapper -->
 
   <?= stick_template('resources/admin_footer') ?>
-
-  <!-- Add the sidebar's background. This div must be placed
-       immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
 
